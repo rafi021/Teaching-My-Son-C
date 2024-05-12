fd91fdc8f4823e554eb56a64b63730b21973049a
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-05-12 22:02:32 +0600

    numeric data types

--- Program.cs
+++ Program.cs
@@ -11,6 +11,33 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Omar bin Ibrahim!");
+
+            int age = 5;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            long bigNumber = 90000000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+
+            float precision = 2566.4455F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+            Console.ReadLine();
         }
     }
 }
