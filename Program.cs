c7420c53937cdc2d4633e657b25eda58007d4db6
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-05-12 23:13:50 +0600

    boolean

--- Program.cs
+++ Program.cs
@@ -10,24 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Omar bin Ibrahim!");
+            string name = "omar bin ibrahim ARAF";
+            bool isChild = true;
 
-            string name = "Omar bin Ibrahim";
-            string textAge = "5";
-            int age = Convert.ToInt32(textAge);
-
-            Console.WriteLine("My text Age: " + textAge);
-            Console.WriteLine("My int Age: " + age);
-            Console.WriteLine(textAge.GetType());
-            Console.WriteLine(age.GetType());
-
-            string textBigNumber = "-900000000000000";
-            long bigNumber = Convert.ToInt64(textBigNumber);
-            Console.WriteLine(textBigNumber);
-            Console.WriteLine(bigNumber.GetType());
-
-
-
+            Console.WriteLine(isChild.GetType()); // 
             Console.ReadLine();
         }
     }
