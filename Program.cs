8b2163e088d53f78d751a43a5fe57825f127729c
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-05-12 22:16:40 +0600

    converting string to numbers

--- Program.cs
+++ Program.cs
@@ -13,14 +13,19 @@ namespace ConsoleApp1
             Console.WriteLine("Hello Omar bin Ibrahim!");
 
             string name = "Omar bin Ibrahim";
-            char letter = 'o';
+            string textAge = "5";
+            int age = Convert.ToInt32(textAge);
 
-            Console.Write("Your name is: ");
-            Console.Write(name);
-            Console.WriteLine();
-            Console.WriteLine(letter);
+            Console.WriteLine("My text Age: " + textAge);
+            Console.WriteLine("My int Age: " + age);
+            Console.WriteLine(textAge.GetType());
+            Console.WriteLine(age.GetType());
 
-            Console.WriteLine(name[3]);
+            string textBigNumber = "-900000000000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(textBigNumber);
+            Console.WriteLine(bigNumber.GetType());
+
 
 
             Console.ReadLine();
